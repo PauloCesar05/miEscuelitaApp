9e074fd1194e7c002feb49d1673345ef200a929e
Sebas <sebasproyecto511@gmail.com>
2022-10-29 14:12:59 -0600

    Registro de Alumno

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AlumnoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +17,4 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'home']);
+Route::get('/alumnos', [AlumnoController::class, 'consultar']);
